2de672a817bedaaf5200d3230ff80b4c8069ec5c
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-01 20:51:24 +0530

    Added comments

--- google-calendar/src/components/Day.js
+++ google-calendar/src/components/Day.js
@@ -3,18 +3,22 @@ import React from "react";
 import dayjs from "dayjs";
 
 export default function Day({ day, rowIdx }) {
+  //Highlights the current day
   function getCurrentDayClass() {
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
       ? "bg-blue-600 text-white round-full w-7"
       : "";
   }
+
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
+        {/* Row for names of the days */}
         {rowIdx === 0 && (
           <p className="text-sm mt-1"> {day.format("ddd").toUpperCase()} </p>
         )}
 
+        {/* Rows for all days and dates */}
         <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
           {day.format("DD")}
         </p>
