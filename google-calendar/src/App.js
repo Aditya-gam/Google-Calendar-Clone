399ac0a002d982b82a1c0695d1363712a4584982
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-01 20:49:58 +0530

    Added global contexts

--- google-calendar/src/App.js
+++ google-calendar/src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 import { getMonth } from "./util";
 import CalendarHeader from "./components/CalendarHeader";
 import Sidebar from "./components/Sidebar";
 import Month from "./components/Month";
+import GlobalContext from "./context/GlobalContext";
 
 import "./App.css";
 
 function App() {
+  // Current month state
   const [currentMonth, setCurrentMonth] = useState(getMonth());
+
+  // Month context
+  const { monthIndex } = useContext(GlobalContext);
+
+  // Change global month context
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
   return (
     <React.Fragment>
-      <div className="h-screen flex flex-columns">
+      <div className="h-screen flex flex-col">
         <CalendarHeader />
+
         <div className="flex flex-1">
           <Sidebar />
           <Month month={currentMonth} />
