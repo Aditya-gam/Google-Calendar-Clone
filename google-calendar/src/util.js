102357c0ecc9f61eadd7a06c2302088c42263983
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-01 20:52:58 +0530

    Added comments

--- google-calendar/src/util.js
+++ google-calendar/src/util.js
@@ -1,10 +1,14 @@
 import dayjs from "dayjs";
 
 export function getMonth(month = dayjs().month()) {
+  month = Math.floor(month);
   const year = dayjs().year();
   const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day();
+
+  // Wrap around the month to proceed to next and previous years
   let currentMonthCount = 0 - firstDayOfTheMonth;
 
+  // Create a 5 X 7 matrix to display dates in the month
   const daysMatrix = new Array(5).fill([]).map(() => {
     return new Array(7).fill(null).map(() => {
       currentMonthCount++;
