507c0f0c0675beac42b2de60124cd095e46246ac
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-01 20:51:40 +0530

    Added comments

--- google-calendar/src/components/Month.js
+++ google-calendar/src/components/Month.js
@@ -5,6 +5,7 @@ import Day from "./Day";
 export default function Month({ month }) {
   return (
     <div className="flex-1 grid grid-cols-7 grid-rows-5">
+      {/* Function to map all the days and dates in a grid format */}
       {month.map((row, i) => (
         <React.Fragment key={i}>
           {row.map((day, idx) => (
