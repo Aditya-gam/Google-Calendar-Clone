3b38c5f51760e290f5fcb37d77882cd0349f97b3
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-02 16:10:34 +0530

    Added functions to display and filter events

--- google-calendar/src/components/Day.js
+++ google-calendar/src/components/Day.js
@@ -1,17 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import dayjs from "dayjs";
 import GlobalContext from "../context/GlobalContext";
 
 export default function Day({ day, rowIdx }) {
+  const [dayEvents, setDayEvents] = useState([]);
+
+  // Read global contexts
+  const {
+    setDaySelected,
+    setShowEventModal,
+    filteredEvents,
+    setSelectedEvent,
+  } = useContext(GlobalContext);
+
+  // Display events on the calendar
+  useEffect(() => {
+    const events = filteredEvents.filter(
+      (evt) => dayjs(evt.day).format("DD-MM-YY") === day.format("DD-MM-YY")
+    );
+    setDayEvents(events);
+  }, [filteredEvents, day]);
+
   //Highlights the current day
   function getCurrentDayClass() {
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
       ? "bg-blue-600 text-white round-full w-7"
       : "";
   }
-
-  const { setDaySelected, setShowEventModal } = useContext(GlobalContext);
 
   return (
     <div className="border border-gray-200 flex flex-col">
@@ -34,7 +50,18 @@ export default function Day({ day, rowIdx }) {
           setDaySelected(day);
           setShowEventModal(true);
         }}
-      ></div>
+      >
+        {/* Shows existing events for that date */}
+        {dayEvents.map((evt, idx) => (
+          <div
+            key={idx}
+            onClick={() => setSelectedEvent(evt)}
+            className={`bg-${evt.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
+          >
+            {evt.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
