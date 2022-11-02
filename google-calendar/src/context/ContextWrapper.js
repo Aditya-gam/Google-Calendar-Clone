896e03cb5c0f44aa744ba4fde25a2a532249313d
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-02 16:14:58 +0530

    Added new states and functions
    New states for selected event, labels, saved events. Function to filter events according to color labels. Store new created and updated events. Display list of labels according to color. Reset create event window when the window is closed.

--- google-calendar/src/context/ContextWrapper.js
+++ google-calendar/src/context/ContextWrapper.js
@@ -1,13 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useReducer, useState } from "react";
 
 import GlobalContext from "./GlobalContext";
 import dayjs from "dayjs";
 
+// CRUD operations for event storage
+function savedEventsReducer(state, { type, payload }) {
+  switch (type) {
+    case "push":
+      return [...state, payload];
+    case "update":
+      return state.map((evt) => (evt.id === payload.id ? payload : evt));
+    case "delete":
+      return state.filter((evt) => evt.id !== payload.id);
+    default:
+      throw new Error();
+  }
+}
+
+// Initialise events at the end of existing list
+function initEvents() {
+  const storageEvents = localStorage.getItem("savedEvents");
+  const parsedEvents = storageEvents ? JSON.parse(storageEvents) : [];
+
+  return parsedEvents;
+}
+
 export default function ContextWrapper(props) {
+  // Global context states
   const [monthIndex, setMonthIndex] = useState(dayjs().month());
   const [smallCalendarMonth, setSmallCalendarMonth] = useState(null);
   const [daySelected, setDaySelected] = useState(dayjs());
   const [showEventModal, setShowEventModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [labels, setLabels] = useState([]);
+  // Storage state
+  const [savedEvents, dispatchCalEvent] = useReducer(
+    savedEventsReducer,
+    [],
+    initEvents
+  );
+
+  // Filter events according to the checkbox
+  const filteredEvents = useMemo(() => {
+    return savedEvents.filter((evt) =>
+      labels
+        .filter((lbl) => lbl.checked)
+        .map((lbl) => lbl.label)
+        .includes(evt.label)
+    );
+  }, [savedEvents, labels]);
+
+  // Store events to local storage
+  useEffect(() => {
+    localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
+  }, [savedEvents]);
+
+  // List of all existing colour labels
+  useEffect(() => {
+    setLabels((prevLabels) => {
+      return [...new Set(savedEvents.map((evt) => evt.label))].map((label) => {
+        const currentLabel = prevLabels.find((lbl) => lbl.label === label);
+        return {
+          label,
+          checked: currentLabel ? currentLabel.checked : true,
+        };
+      });
+    });
+  }, [savedEvents]);
 
   // Sync large and small calendars
   useEffect(() => {
@@ -16,17 +75,45 @@ export default function ContextWrapper(props) {
     }
   }, [smallCalendarMonth]);
 
+  // Reset event modal window after closing it to empty
+  useEffect(() => {
+    if (!showEventModal) {
+      setSelectedEvent(null);
+    }
+  }, [showEventModal]);
+
+  // Check and uncheck the label list filter
+  function updateLabel(label) {
+    setLabels(labels.map((lbl) => (lbl.label === label.label ? label : lbl)));
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         monthIndex,
         setMonthIndex,
+
         smallCalendarMonth,
         setSmallCalendarMonth,
+
         daySelected,
         setDaySelected,
+
         showEventModal,
         setShowEventModal,
+
+        dispatchCalEvent,
+        savedEvents,
+
+        selectedEvent,
+        setSelectedEvent,
+
+        labels,
+        setLabels,
+
+        updateLabel,
+
+        filteredEvents,
       }}
     >
       {props.children}
