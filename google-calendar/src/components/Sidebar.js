9965d73c4bdf73c734049d89d4e3a148f58bda32
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-02 16:12:21 +0530

    Added labels list

--- google-calendar/src/components/Sidebar.js
+++ google-calendar/src/components/Sidebar.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import CreateEventButton from "./CreateEventButton";
 import Smallcalendar from "./Smallcalendar";
+import Labels from "./Labels";
 
 export default function Sidebar() {
   return (
     <aside className="border p-5 w-64">
       <CreateEventButton />
       <Smallcalendar />
+      <Labels />
     </aside>
   );
 }
